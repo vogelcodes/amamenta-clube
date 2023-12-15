fcb18fb2769b6ff7d68565dc63b02780769215ec
vogelcodes <54858106+vogelcodes@users.noreply.github.com>
2023-12-15 04:57:10 +0000

    `database.js`

---
+++ infra/database.js
@@ -0,0 +1,18 @@
+import { Client } from "pg";
+async function query() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  await client.connect();
+  const res = await client.query("SELECT 1 + 1 as Sum;");
+  console.log(res.rows[0]);
+  return res;
+}
+
+export default {
+  query: query,
+};
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,8 @@
-function status(request, response) {
-  response.status(200).json({ server: "OK" });
+import database from "../../../../infra/database";
+
+async function status(request, response) {
+  const result = await database.query();
+  response.status(200).json({ server: "OK", db_status: result.rows[0] });
 }
 
 export default status;
