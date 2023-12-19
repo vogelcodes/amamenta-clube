6d7d50b45883a44f024a0f82c833795fa8036f2d
vogelcodes <54858106+vogelcodes@users.noreply.github.com>
2023-12-19 16:13:04 +0000

    SQL insjection protection, finishing status API endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,18 +2,23 @@ import database from "infra/database";
 
 async function status(request, response) {
   const updatedAt = new Date();
+
   const databaseVersion = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersion.rows[0].server_version;
   const databaseMaxConnectionsResult = await database.query(
     "SHOW max_connections;",
   );
-  const databaseOpenedConnectionsResult = await database.query(
-    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
-  );
+  const databaseName = process.env.POSTGRES_DB;
+  console.log(databaseName);
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
   const databaseMaxConnectionsValue =
     databaseMaxConnectionsResult.rows[0].max_connections;
   const databaseOpenedConnectionsValue =
     databaseOpenedConnectionsResult.rows[0].count;
+  console.log(databaseOpenedConnectionsValue);
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,7 +2,6 @@ test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
   const responseBody = await response.json();
-  console.log(responseBody);
   expect(responseBody.updated_at).toBeDefined();
   expect(responseBody.dependencies.database.version).toBeDefined();
   expect(responseBody.dependencies.database.max_connections).toBeDefined();
